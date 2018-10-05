d054a465313c185abd93f3401ab90c23a8c8a533
Unknown <t.kurylo@gmail.com>
2018-10-05 13:27:58 +0200

    Add work done during TP3

---
+++ src/Controller/FrontofficeHomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Circuit;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Controleur FrontOfficeHome
+ * @Route("/")
+ */
+class FrontofficeHomeController extends Controller
+{
+    /**
+     * @Route("/home", name="frontoffice_home")
+     */
+    public function index()
+    {   
+        $em = $this->getDoctrine()->getManager();
+        $circuits = $em->getRepository(Circuit::class)->findAll();
+        dump($circuits);
+        return $this->render('front/home.html.twig', [
+            'circuits' => $circuits, 
+        ]);
+    }
+
+    /**
+     * Finds and displays a circuit entity.
+     *
+     * @Route("/circuit/{id}", name="front_circuit_show")
+     */
+    public function circuitShow($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $circuit = $em->getRepository(Circuit::class)->find($id);
+        dump($circuit);
+        return $this->render('front/circuit_show.html.twig', [
+            'circuit' => $circuit,
+        ]);
+    }
+}
---
+++ src/Entity/Circuit.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CircuitRepository")
+ */
+class Circuit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $paysDepart;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $villeDepart;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $villeArrivee;
+
+    /**
+     * @ORM\Column(type="smallint", nullable=true)
+     */
+    private $dureeCircuit;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Etape", mappedBy="circuit", orphanRemoval=true)
+     */
+    private $etapes;
+
+    public function __construct()
+    {
+        $this->etapes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPaysDepart(): ?string
+    {
+        return $this->paysDepart;
+    }
+
+    public function setPaysDepart(?string $paysDepart): self
+    {
+        $this->paysDepart = $paysDepart;
+
+        return $this;
+    }
+
+    public function getVilleDepart(): ?string
+    {
+        return $this->villeDepart;
+    }
+
+    public function setVilleDepart(?string $villeDepart): self
+    {
+        $this->villeDepart = $villeDepart;
+
+        return $this;
+    }
+
+    public function getVilleArrivee(): ?string
+    {
+        return $this->villeArrivee;
+    }
+
+    public function setVilleArrivee(?string $villeArrivee): self
+    {
+        $this->villeArrivee = $villeArrivee;
+
+        return $this;
+    }
+
+    public function getDureeCircuit(): ?int
+    {
+        return $this->dureeCircuit;
+    }
+
+    public function setDureeCircuit(?int $dureeCircuit): self
+    {
+        $this->dureeCircuit = $dureeCircuit;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Etape[]
+     */
+    public function getEtapes(): Collection
+    {
+        return $this->etapes;
+    }
+
+    public function addEtape(Etape $etape): self
+    {
+        if (!$this->etapes->contains($etape)) {
+            $this->etapes[] = $etape;
+            $etape->setCircuit($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtape(Etape $etape): self
+    {
+        if ($this->etapes->contains($etape)) {
+            $this->etapes->removeElement($etape);
+            // set the owning side to null (unless already changed)
+            if ($etape->getCircuit() === $this) {
+                $etape->setCircuit(null);
+            }
+        }
+
+        return $this;
+    }
+}
